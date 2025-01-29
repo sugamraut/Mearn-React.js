b6c576a031c4650c5b0b887743e48e4943b1b1e9
Sugam Raut <sugamrautbim@gmail.com>
2025-01-29 22:01:22 +0545

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import About from './page/blog/About'
 function App() {
   return (
     <>
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
      <BrowserRouter>
      <Routes>
       <Route path='/' element={<Home/>}/>
--- src/page/blog/Home.jsx
+++ src/page/blog/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1>this is home page</h1>
+      <h1 className="text-blue-500">this is home page</h1>
     </div>
   )
 }
