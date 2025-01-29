63c8a4bac75d0ca4b369288e5acde123d9aa2b8d
Sugam Raut <sugamrautbim@gmail.com>
2025-01-29 21:43:24 +0545

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+    extend: {},
+    },
+    plugins: [],
+    }
+
+    //tailwind configure code npm install -D tailwindcss postcss autoprefixer then make two folder ie tailwind.config and postcss.config
