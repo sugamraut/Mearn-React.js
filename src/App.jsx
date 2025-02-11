0be924690c5db84bdf0c9cf81f5f6436e9388932
Sugam Raut <sugamrautbim@gmail.com>
2025-02-11 22:09:20 +0545

    edit UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './page/blog/home'
 import About from './page/blog/About'
 import SingleBlog from './page/blog/SingleBlog'
 import CreateBlog from './page/blog/CreateBlog'
+import EditBlog from './page/blog/EditBlog'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path='/create' element={<CreateBlog/>} />
       <Route path='/about' element={<About/>}/>
       <Route path='/blog/:id' element={<SingleBlog/>}/>
+      <Route path='/edit/:id' element={<EditBlog/>} />
      </Routes>
      
      </BrowserRouter>
--- src/page/blog/SingleBlog.jsx
+++ src/page/blog/SingleBlog.jsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams, useNavigate, Link } from "react-router-dom"
 import Navbar from "../../components/Navbar"
 import axios from "axios"
 import { useEffect, useState } from "react"
@@ -41,7 +41,9 @@ function SingleBlog(){
                 </div>
                 <div class="flex -mx-2 mb-4">
                     <div class="w-1/2 px-2">
+                    <Link to={`/edit/${blog._id}`}>
                         <button class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit me</button>
+                        </Link>
                     </div>
                     <div class="w-1/2 px-2">
                         <button class="w-full bg-gray-200 dark:bg-red-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteGarneFunction} >Delete me</button>
